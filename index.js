87e6e217d050e79a113cfb4d6aea172975255c0e
Chintu <raxitbhanderi@gmail.com>
2023-01-03 21:04:50 +0530

    simple chat app2

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const port = 3000;
 const expressServer = http.createServer(app);
 
 const {Server} = require('socket.io')
-const io = new Server(expressServer);
+const io = new Server(expressServer)
+
+
+// check socket is working or not...
+// if new user is visite then cb will call.. 
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+})
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/index.html");
