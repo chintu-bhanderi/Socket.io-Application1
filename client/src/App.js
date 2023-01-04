8f82d1b32ea7287e7e37b8f190b78916e9bc4a93
Chintu <raxitbhanderi@gmail.com>
2023-01-04 20:22:12 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
+import io from 'socket.io-client'
 import './App.css';
 
 function App() {
+  const socket = io.connect('/');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is client side socket.</h1>
     </div>
-  );
+  )
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+
 }
 
 export default App;
